9505c221b475ae15d78b014a27d416c242e4e432
dooddles07 <brixdodd07@gmail.com>
2026-07-31 15:02:44 +0800

    feat(auth): gate all /app routes behind sign-in

--- src/ui/layouts/AppLayout.tsx
+++ src/ui/layouts/AppLayout.tsx
@@ -2,30 +2,33 @@ import { Outlet, useLocation } from 'react-router';
 import { BottomNav } from './BottomNav';
 import { Sidebar } from './Sidebar';
 import { OfflineBanner } from './OfflineBanner';
+import { AuthGate } from '@/ui/components/AuthGate';
 import { nav } from '@/copy/labels';
 
 export function AppLayout() {
   const location = useLocation();
 
   return (
-    <div className="min-h-dvh bg-bg">
-      <a href="#main" className="skip-link">
-        {nav.skipToContent}
-      </a>
+    <AuthGate>
+      <div className="min-h-dvh bg-bg">
+        <a href="#main" className="skip-link">
+          {nav.skipToContent}
+        </a>
 
-      <Sidebar />
+        <Sidebar />
 
-      <div className="lg:pl-60">
-        <OfflineBanner />
-        {/* Bottom padding clears the tab bar; it is only present below lg. */}
-        <main id="main" tabIndex={-1} className="pb-28 lg:pb-12">
-          <div key={location.pathname} className="route-fade">
-            <Outlet />
-          </div>
-        </main>
+        <div className="lg:pl-60">
+          <OfflineBanner />
+          {/* Bottom padding clears the tab bar; it is only present below lg. */}
+          <main id="main" tabIndex={-1} className="pb-28 lg:pb-12">
+            <div key={location.pathname} className="route-fade">
+              <Outlet />
+            </div>
+          </main>
+        </div>
+
+        <BottomNav />
       </div>
-
-      <BottomNav />
-    </div>
+    </AuthGate>
   );
 }
